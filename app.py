3f4eee4df28377b8eb81790649133a4fae05ee2e
fatoumataseck <fatouseck00@gmail.com>
2019-08-17 19:34:26 +0000

    gestion bouton ancien et nouveau

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask,render_template,request,redirect,url_for,flash,session, escape,jsonify
+from flask import Flask,render_template,request,redirect,url_for,flash,jsonify
 from flask_sqlalchemy import *
 from sqlalchemy import *
 import datetime
 from wtforms import *
 from flask_wtf import FlaskForm
+
 app= Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:fatou@localhost/form_etudiant'
 db = SQLAlchemy(app)
@@ -26,16 +27,18 @@ class Etudiant(db.Model):
         self.nom=nom
         self.dn=dn
     def __repr__(self):
-        return '<etudiant %r>' % self.matricule,self.nom,self.prenom,self.dn
+        return '<etudiant %r,%r,%r,%r>' % (self.matricule,self.nom,self.prenom,self.dn)
 
 class Filiere(db.Model):
     __tablename__='filiere'
     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     libelle_filiere = db.Column(db.String(80), unique=True, nullable=False)
-    def __init__(self,libelle_filiere):
+    def __init__(self,id,libelle_filiere):
+        self.id=id
         self.libelle_filiere=libelle_filiere
+        
     def __repr__(self):
-        return '<filiere %r>' % self.libelle_filiere
+        return '<filiere %r ,%r>' % (self.id,self.libelle_filiere)
 
 class Classe(db.Model):
     __tablename__='classe'
@@ -69,32 +72,66 @@ class Inscription(db.Model):
 
 db.create_all()
 
-class Form(FlaskForm):
-    id_filiere=SelectField('id_filiere',choices=[(1,'AI'),(2,'developpement web'),(3,'data artisan'),(4,'referent digital')])
-    classe=SelectField('classe',choices=[])
-    montant_ins=TextField('montant_ins')
-
-
-
 #*****************************************************************************************************************
 #                                    PAGE FORMULAIRE                                      
 #***************************************************************************************************************
-@app.route('/',methods=['GET','POST'] )
-def acc():
+
+#****************************************** generation matricule ************************************************
+@app.route('/matricule')
+def matricule(): 
     today = datetime.date.today()
     y=today.year
     etu=db.session.query(func.max(Etudiant.id)).one() 
-    mat="SA-"+str(y)+"-"+str(etu[0])
-    fi=Filiere.query.all()
-    clas=Classe.query.all()
-    form=Form()
-    form.classe.choices=[(classe.id,classe.libelle_classe) for classe in Classe.query.filter_by(id_filiere=1).all()]
+    if etu[0]==None:
+        mat="SA-"+str(y)+"-"+str(1)
+        id_etu=1
+    else:
+        mat="SA-"+str(y)+"-"+str(etu[0]+1)
+        id_etu=int(etu[0]+1)
+    matArray=[]
+    matArray.append(mat)
+    return jsonify({'etu' : matArray})
+
+#************************************************** remplissage formulaire ******************************************************
+
+@app.route('/',methods=['GET','POST'] )
+def acc():
+    etu=db.session.query(func.max(Etudiant.id)).one() 
+    if etu[0]==None:
+        id_etu=1
+    else:
+        id_etu=int(etu[0]+1)
+    fi=db.session.query(Filiere.id,Filiere.libelle_filiere).all()
     if request.method == 'POST':
-        classe = Classe.query.filter_by(id=form.classe.data).first()
-        return '<h3>filiere:{},classe:{},montant_ins:{},mensualite:{}</h3>'.format(form.id_filiere.data, classe.libelle_classe,classe.montant_ins,classe.mensualite)
+        matricule=request.form['matricule']
+        nom=request.form['nom']
+        prenom=request.form['prenom']
+        dn=request.form['dn']
+        db.session.add(Etudiant(matricule,nom,prenom,dn))
+        db.session.commit() 
         
-    return render_template('pages/formulaire.html',mat=mat,fi=fi,clas=clas,form=form)
- 
+        date_ins=request.form['date_ins']
+        annee_aca=request.form['annee_aca']
+        id_classe=request.form['classe']
+        db.session.add(Inscription(date_ins,annee_aca,id_etu,id_classe))
+        db.session.commit() 
+        return redirect(url_for('acc'))
+        
+    return render_template('pages/formulaire.html',fi=fi)
+
+#**************************************** chargement filiere *******************************************************
+@app.route('/filiere')
+def filiere(): 
+    filieres=db.session.query(Filiere.id,Filiere.libelle_filiere).all()
+    filiereArray = []
+    for filiere in filieres:
+        filiereObj = {}
+        filiereObj['id'] = filiere.id
+        filiereObj['libelle_filiere'] = filiere.libelle_filiere
+        filiereArray.append(filiereObj)
+    return jsonify({'filieres' : filiereArray})
+
+#**************************************** chargement classe *******************************************************
 @app.route('/classe/<id_filiere>')
 def classe(id_filiere): 
     classes = Classe.query.filter_by(id_filiere=id_filiere).all()
@@ -105,14 +142,26 @@ def classe(id_filiere):
         classeObj['libelle_classe'] = classe.libelle_classe
         classeObj['montant_ins'] = classe.montant_ins
         classeObj['mensualite'] = classe.mensualite
-
         classeArray.append(classeObj)
     return jsonify({'classes' : classeArray})
 
-
-
-
-
+@app.route('/recherche/<mat>')
+def recherche(mat): 
+    etudiants=db.session.query(Etudiant.matricule,Etudiant.prenom,Etudiant.nom,Etudiant.dn,Inscription.date_ins, Inscription.id_classe,Classe.id_filiere,Classe.libelle_classe,Filiere.libelle_filiere).join(Inscription, Inscription.id_etu == Etudiant.id).join(Classe,Classe.id==Inscription.id_classe).join(Filiere, Filiere.id == Classe.id_filiere).filter(Etudiant.matricule==mat).all()
+    print(etudiants)
+    recherchearray = []
+    for etudiant in etudiants:
+        etudiantObj = {}
+        etudiantObj['matricule'] = etudiant.matricule
+        etudiantObj['nom'] = etudiant.nom
+        etudiantObj['prenom'] = etudiant.prenom
+        etudiantObj['dn'] = etudiant.dn
+        etudiantObj['id_classe'] = etudiant.id_classe
+        etudiantObj['libelle_filiere'] = etudiant.libelle_filiere
+        etudiantObj['id_filiere'] = etudiant.id_filiere
+        etudiantObj['libelle_classe'] = etudiant.libelle_classe
+        recherchearray.append(etudiantObj)
+    return jsonify({'etudiants' : recherchearray})
 
 
 if __name__ == "__main__":
